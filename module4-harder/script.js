2276ccbcecfaeb177e548d798b2783899645e506
John Emmerechts <john093e@gmail.com>
2016-09-21 01:26:22 +0200

    module4-harder

--- module4-harder/script.js
+++ module4-harder/script.js
@@ -1,14 +1,11 @@
 (function (){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-  var str = names[i];
-  var res = str.charAt(0);
-  var res = res.toLowerCase();
-  if (res === "j") {
-    console.log(byeSpeaker.speak(names[i]));
-  } 
-  else {
-    console.log(helloSpeaker.speak(names[i]));
-  }
+for (var name in names) {
+	var firstLetter = names[name].toLowerCase().charAt(0);
+  if (firstLetter === 'j') {
+      byeSpeaker.speak(names[name]);
+    } else {
+      helloSpeaker.speak(names[name]);
+    }
 }
 })();
